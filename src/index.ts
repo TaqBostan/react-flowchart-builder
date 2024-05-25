8c9c14c841d2bddd835d11ee187f1d11cb827e24
TaqBostan <mmehdi_rajaee@yahoo.com>
2024-05-25 13:31:12 +0330

    updating exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { ConnectorData, NodeData } from './base/types';
 import { Flowchart } from './flowchart-builder';
-import { useFlowchart } from './flowchart-builder/hook';
-export { Flowchart, useFlowchart }
+import { useFlowchart, FlowchartHandles } from './flowchart-builder/hook';
+export { Flowchart, useFlowchart, FlowchartHandles, ConnectorData, NodeData }
