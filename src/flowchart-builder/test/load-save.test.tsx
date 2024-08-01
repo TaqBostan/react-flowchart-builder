87bfc24b2a05886eaf7067fd2e951b36629ea196
TaqBostan <mmehdi_rajaee@yahoo.com>
2024-08-01 18:45:42 +0330

    test for getData

--- src/flowchart-builder/test/load-save.test.tsx
+++ src/flowchart-builder/test/load-save.test.tsx
@@ -1,4 +1,4 @@
-import { cleanup, fireEvent, render, prettyDOM, renderHook } from '@testing-library/react';
+import { cleanup, fireEvent, render, prettyDOM, renderHook, waitFor } from '@testing-library/react';
 import React from 'react';
 import '@testing-library/jest-dom';
 import { Flowchart } from '../index';
@@ -7,30 +7,58 @@ import { LinkData, NodeData } from '../../base/types';
 
 export const ns = "http://www.w3.org/2000/svg";
 
-afterEach(cleanup);
-
 Object.defineProperty(global.SVGElement.prototype, 'getBBox', {
   writable: true,
   value: jest.fn().mockReturnValue({
     x: 0,
     y: 0,
     width: 0,
-    height:0,
+    height: 0,
   }),
+});
+
+it('Save: circle-rect link', async () => {
+  let circle = JSON.parse(`{ "id": 15, "X": 91, "Y": 119, "text": "Start", "shape": "circle" }`);
+  let rect = JSON.parse(`{ "id": 20, "X": 96, "Y": 254, "text": "Shopping", "color": "red" }`);
+  let link = JSON.parse(`{ "from": 15, "to": 20, "text": "Visit online store" }`);
+  const onReady = (flowchart: FlowchartHandles) => {
+    flowchart.addNodes([circle, rect], [link]);
+  }
+  const res = renderHook(useFlowchart);
+  const _flowchart = render(
+    <Flowchart setHandles={res.result.current.setHandles} width='1200px' height='600px' editable={true} onReady={onReady} />,
+  );
+  let {nodes, links} = res.result.current.flowchart!.getData();
+  expect(nodes.length).toBe(2);
+  expect(links.length).toBe(1);
+
+  let n1 = nodes.find(n => n.id === 15)!, n2 = nodes.find(n => n.id === 20)!, lnk = links[0];
+  expect(n1.X).toBe(91);
+  expect(n1.Y).toBe(119);
+  expect(n1.text).toBe('Start');
+  expect(n1.shape).toBe('circle');
+  expect(n1.color).toBe('transparent');
+  expect(n2.X).toBe(96);
+  expect(n2.Y).toBe(254);
+  expect(n2.text).toBe('Shopping');
+  expect(n2.shape).toBe('rectangle');
+  expect(n2.color).toBe('red');
+  expect(lnk.from).toBe(15);
+  expect(lnk.to).toBe(20);
+  expect(lnk.text).toBe("Visit online store");
+  expect(lnk.type).toBe("solid");
+  
 });
 
 it('Load: circle-rect link', () => {
   let circle = JSON.parse(`{ "id": 1, "X": 91, "Y": 119, "text": "Start", "shape": "circle", "color": "transparent" }`);
   let rect = JSON.parse(`{ "id": 2, "X": 96, "Y": 254, "text": "Shopping", "shape": "rectangle", "color": "transparent" }`);
   let link = JSON.parse(`{ "from": 1, "to": 2, "text": "Visit online store" }`);
-  let hk = renderHook(() => React.useState<{ nodes: NodeData[], links: LinkData[] }>({ nodes: [], links: [] }));
-  const [data, setData] = hk.result.current;
   const onReady = (flowchart: FlowchartHandles) => {
     flowchart.addNodes([circle, rect], [link]);
-    setData(flowchart.getData());
   }
   const res = renderHook(useFlowchart);
-  let { setHandles, flowchart } = res.result.current;
+  let { setHandles } = res.result.current;
   const _flowchart = render(
     <Flowchart setHandles={setHandles} width='1200px' height='600px' editable={true} onReady={onReady} />,
   );
@@ -45,21 +73,18 @@ it('Load: circle-rect link', () => {
   let arrow = groups[2].querySelector('path:not(.connector)')!;
   expect(arrow.getAttribute('d')).toBe('M0,0l-9,5v-10l9,5');
   expect(arrow.getAttribute('transform')).toBe('translate(113,254) rotate(109.64844669725134)');
-  
+
   expect(_flowchart.getByText('Visit online store')).toBeInTheDocument();
 });
 
 it('Load: circle', () => {
   let nodes = JSON.parse(`[{ "id": 20, "X": 91, "Y": 119, "text": "Start", "shape": "circle", "color": "transparent" }]`);
   let links: LinkData[] = [];
-  let hk = renderHook(() => React.useState<{ nodes: NodeData[], links: LinkData[] }>({ nodes: [], links: [] }));
-  const [data, setData] = hk.result.current;
   const onReady = (flowchart: FlowchartHandles) => {
     flowchart.addNodes(nodes, links);
-    setData(flowchart.getData());
   }
   const res = renderHook(useFlowchart);
-  let { setHandles, flowchart } = res.result.current;
+  let { setHandles } = res.result.current;
   const _flowchart = render(
     <Flowchart setHandles={setHandles} width='1200px' height='600px' editable={true} onReady={onReady} />,
   );
@@ -77,14 +102,11 @@ it('Load: circle', () => {
 it('Load: rect', () => {
   let nodes = JSON.parse(`[{ "id": 22, "X": 96, "Y": 254, "text": "Shopping", "shape": "rectangle", "color": "transparent" }]`);
   let links: LinkData[] = [];
-  let hk = renderHook(() => React.useState<{ nodes: NodeData[], links: LinkData[] }>({ nodes: [], links: [] }));
-  const [data, setData] = hk.result.current;
   const onReady = (flowchart: FlowchartHandles) => {
     flowchart.addNodes(nodes, links);
-    setData(flowchart.getData());
   }
   const res = renderHook(useFlowchart);
-  let { setHandles, flowchart } = res.result.current;
+  let { setHandles } = res.result.current;
   const _flowchart = render(
     <Flowchart setHandles={setHandles} width='1200px' height='600px' editable={true} onReady={onReady} />,
   );
@@ -102,14 +124,11 @@ it('Load: rect', () => {
 it('Load: rhombus', () => {
   let nodes = JSON.parse(`[{ "id": 27, "X": 728, "Y": 168, "text": "Item Selected?", "shape": "rhombus", "color": "transparent" }]`);
   let links: LinkData[] = [];
-  let hk = renderHook(() => React.useState<{ nodes: NodeData[], links: LinkData[] }>({ nodes: [], links: [] }));
-  const [data, setData] = hk.result.current;
   const onReady = (flowchart: FlowchartHandles) => {
     flowchart.addNodes(nodes, links);
-    setData(flowchart.getData());
   }
   const res = renderHook(useFlowchart);
-  let { setHandles, flowchart } = res.result.current;
+  let { setHandles } = res.result.current;
   const _flowchart = render(
     <Flowchart setHandles={setHandles} width='1200px' height='600px' editable={true} onReady={onReady} />,
   );
@@ -127,14 +146,11 @@ it('Load: rhombus', () => {
 it('editable true', () => {
   let nodes = JSON.parse(`[{ "id": 20, "X": 91, "Y": 119, "text": "Start", "shape": "circle", "color": "transparent" }]`);
   let links: LinkData[] = [];
-  let hk = renderHook(() => React.useState<{ nodes: NodeData[], links: LinkData[] }>({ nodes: [], links: [] }));
-  const [data, setData] = hk.result.current;
   const onReady = (flowchart: FlowchartHandles) => {
     flowchart.addNodes(nodes, links);
-    setData(flowchart.getData());
   }
   const res = renderHook(useFlowchart);
-  let { setHandles, flowchart } = res.result.current;
+  let { setHandles } = res.result.current;
   const _flowchart = render(
     <Flowchart setHandles={setHandles} width='1200px' height='600px' editable={true} onReady={onReady} />,
   );
@@ -147,14 +163,11 @@ it('editable true', () => {
 it('editable false', () => {
   let nodes = JSON.parse(`[{ "id": 20, "X": 91, "Y": 119, "text": "Start", "shape": "circle", "color": "transparent" }]`);
   let links: LinkData[] = [];
-  let hk = renderHook(() => React.useState<{ nodes: NodeData[], links: LinkData[] }>({ nodes: [], links: [] }));
-  const [data, setData] = hk.result.current;
   const onReady = (flowchart: FlowchartHandles) => {
     flowchart.addNodes(nodes, links);
-    setData(flowchart.getData());
   }
   const res = renderHook(useFlowchart);
-  let { setHandles, flowchart } = res.result.current;
+  let { setHandles } = res.result.current;
   const _flowchart = render(
     <Flowchart setHandles={setHandles} width='1200px' height='600px' editable={false} onReady={onReady} />,
   );
